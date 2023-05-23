6043a7f36c62c220ed05174d5b72e28b0446cfa3
Code_IDE <code.ide.07@gmail.com>
2023-05-23 21:46:24 +0530

    New Project

---
+++ CodeIDE/src/main/java/com/codeIDe/CodeIDE/CodeIdeApplication.java
@@ -0,0 +1,14 @@
+package com.codeIDe.CodeIDE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeIdeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeIdeApplication.class, args);
+	}
+
+}
+
---
+++ CodeIDE/src/test/java/com/codeIDe/CodeIDE/CodeIdeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeIDe.CodeIDE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeIdeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
